7f31c3e0ef6642847213cfcffe2f082fe5107d74
LeKiSHviLi1 <beqalekishvili@gmail.com>
2023-12-21 01:32:26 +0400

    some  design changes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
 var i = 0;
 
-function add_task() {
 
+function add_task() {
+    
 var new_task = document.getElementById("add_new_task");
 
-var form_content = document.createElement("div");
+var form_content = document.createElement("form");
 var checkbox = document.createElement("input");
 
 
 checkbox.type = "checkbox";
 checkbox.name = "checkbox"+(i + 1);
 checkbox.id = "checkbox" + (i + 1);
+checkbox.className = "checkbox";
 
 var label = document.createElement("label");
 label.innerHTML = new_task.value;
 label_id = "checkbox"+(i + 1);
 label.setAttribute("for",label_id);
 
+form_content.id = "form_content"+(i+1);
+form_content.className = "form_content"
 var breake = document.createElement("br");
 
 
-
 var task_content = document.getElementById("class_content")
-var form_content = document.getElementById("form_content");
 
 document.body.appendChild(task_content);
 task_content.appendChild(form_content);
@@ -32,5 +34,11 @@ form_content.appendChild(label);
 form_content.appendChild(breake);
 
 i=i+1;
+clearLable();
+}
 
+
+
+function clearLable(){
+    document.getElementById("add_new_task").value = '';
 }
